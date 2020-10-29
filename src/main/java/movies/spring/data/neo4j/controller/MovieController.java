bfebe53b445a7305cb3f3062ea6ed17820e3e98b
SANOGO <SOULEYSANOGO@gs2e.ci>
2020-10-29 16:17:42 +0000

    Update

---
+++ src/main/java/movies/spring/data/neo4j/controller/MovieController.java
@@ -0,0 +1,49 @@
+package movies.spring.data.neo4j.controller;
+
+import java.util.Collection;
+import java.util.Map;
+
+import movies.spring.data.neo4j.domain.Movie;
+import movies.spring.data.neo4j.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mark Angrish
+ */
+@RestController
+@RequestMapping("/")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+	
+	@PostMapping(path = "/movies")
+    public void addMovies(@RequestBody Movie movie) {
+        //code
+    }
+ 
+
+    @GetMapping("/movie")
+    public Movie findByTitle(@RequestParam String title) {
+        return movieService.findByTitle(title);
+    }
+
+    @GetMapping("/movies")
+    public Collection<Movie> findByTitleLike(@RequestParam String title) {
+        return movieService.findByTitleLike(title);
+    }
+
+    @GetMapping("/graph")
+    public Map<String, Object> graph(@RequestParam(value = "limit", required = false) Integer limit) {
+        return movieService.graph(limit == null ? 100 : limit);
+    }
+}
